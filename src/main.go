ef76fdae52bede9c9ad4d3faf6a7feda745cc4e6
Stephen Wayne <stephenwayne@Stephens-MacBook-Pro.local>
2022-02-25 13:12:11 -0700

    initial loadbalancer

---
+++ src/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+)
+
+type Server interface {
+	// Address returns the address with which to access the server
+	Address() string
+
+	// CheckAlive returns true if the server is alive and able to serve requests
+	IsAlive() bool
+}
+
+type simpleServer struct {
+	addr string
+}
+
+func (s *simpleServer) Address() string { return s.addr }
+
+func (s *simpleServer) IsAlive() bool { return true }
+
+type LoadBalancer struct {
+	port            string
+	roundRobinCount int
+	servers         []Server
+}
+
+func NewLoadBalancer(port string, servers []Server) *LoadBalancer {
+	return &LoadBalancer{
+		port:            port,
+		roundRobinCount: 0,
+		servers:         servers,
+	}
+}
+
+// getNextServerAddr returns the address of the next available server to send a
+// request to, using a simple round-robin algorithm
+func (lb *LoadBalancer) getNextAvailableServer() Server {
+	server := lb.servers[lb.roundRobinCount%len(lb.servers)]
+	for !server.IsAlive() {
+		lb.roundRobinCount++
+		server = lb.servers[lb.roundRobinCount%len(lb.servers)]
+	}
+	lb.roundRobinCount++
+
+	return server
+}
+
+// handleErr prints the error and exits the program
+// Note: this is not how one would want to handle errors in production, but
+// serves well for demonstration purposes.
+func handleErr(err error) {
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func (lb *LoadBalancer) serveProxy(rw http.ResponseWriter, req *http.Request) {
+	targetServer := lb.getNextAvailableServer()
+
+	targetUrl, err := url.Parse(targetServer.Address())
+	handleErr(err)
+
+	// could optionally log stuff about the request here!
+	fmt.Printf("forwarding request to address %q\n", targetServer.Address())
+
+	// could delete pre-existing X-Forwarded-For header to prevent IP spoofing
+	proxy := httputil.NewSingleHostReverseProxy(targetUrl)
+	proxy.ServeHTTP(rw, req)
+}
+
+func main() {
+	servers := []Server{
+		&simpleServer{
+			addr: "https://www.google.com",
+		},
+		&simpleServer{
+			addr: "https://www.bing.com",
+		},
+		&simpleServer{
+			addr: "https://www.duckduckgo.com",
+		},
+	}
+	lb := NewLoadBalancer("8000", servers)
+	handleRedirect := func(rw http.ResponseWriter, req *http.Request) {
+		lb.serveProxy(rw, req)
+	}
+
+	// register a proxy handler to handle all requests
+	http.HandleFunc("/", handleRedirect)
+
+	http.ListenAndServe(":"+lb.port, nil)
+}
